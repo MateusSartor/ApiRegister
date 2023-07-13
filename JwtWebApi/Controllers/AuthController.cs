defcd0073ea74648bd0506d106bc31a05f63dc8b
Mateus_Sartor <mateus.sartor5@gmail.com>
2023-07-13 16:34:26 -0300

    Add project files.

---
+++ JwtWebApi/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration) 
+        {
+            _configuration = configuration;  
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request) 
+        {
+        
+
+            user.Username = request.Username;
+            user.Password = request.Password;
+
+            return Ok(user);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+           if (user.Username != request.Username)
+            {
+                return BadRequest("Usuário não encontrado. ");
+            }
+                
+           if (user.Password != request.Password)
+            {
+                return BadRequest("Senha Incorreta");
+            }
+
+
+            return Ok("Login efeituado com sucesso");
+        
+        }
+   
+        }
+
+    }
+
